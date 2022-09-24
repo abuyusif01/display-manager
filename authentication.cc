fab930fde3b05220e89301eac6298a38a7711fb6
abuyusif01 <abubakaryusuf951@gmail.com>
2022-09-24 12:55:59 +0800

    authentication: fix env variables
    Signed-off-by: abuyusif01 <abubakaryusuf951@gmail.com>

--- authentication.cc
+++ authentication.cc
@@ -1,16 +1,6 @@
 #include "headers/authentication.h"
 #include "headers/logger.h"
 #include "headers/ui.h"
-#include <unistd.h>
-
-#include <dirent.h>
-#include <limits.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
 
 static pam_handle_t *pam_handle;
 
@@ -153,12 +143,6 @@ bool Authentication::set_env(std::string nam, std::string value)
 bool Authentication::logout(void)
 {
 
-    /*
-    1 -> ERROR
-    2 -> WARNING
-    3 -> INFO
-    */
-
     int result = pam_close_session(pam_handle, 0);
     if (result != PAM_SUCCESS)
     {
@@ -199,46 +183,6 @@ void Authentication::xorg(struct passwd *pw, const char *vt, const char *desktop
         snprintf(x_cmd, 1024, "%s", "/usr/bin/startx");
         execl(pw->pw_shell, pw->pw_shell, "-c", x_cmd, NULL);
         exit(EXIT_SUCCESS);
-    }
-
-    int ok;
-    xcb_connection_t *xcb;
-
-    do
-    {
-        xcb = xcb_connect(NULL, NULL);
-        ok = xcb_connection_has_error(xcb);
-        kill(pid, 0);
-    } while ((ok != 0) && (errno != ESRCH));
-
-    if (ok != 0)
-    {
-        return;
-    }
-
-    pid_t xorg_pid = fork();
-
-    if (xorg_pid == 0)
-    {
-        chdir(pw->pw_dir);
-        // char *_cmd = "exec /bin/bash --login .xinitrc";
-        char de_cmd[1024];
-
-        snprintf(de_cmd, 1024, "%s %s", "/etc/ly/xsetup.sh", desktop_cmd);
-        Logger(-1, de_cmd);
-        execl(pw->pw_shell, pw->pw_shell, "-c", de_cmd, NULL);
-        exit(EXIT_SUCCESS);
-    }
-
-    int status;
-    waitpid(xorg_pid, &status, 0);
-    // xcb_disconnect(xcb);
-    kill(pid, 0);
-
-    if (errno != ESRCH)
-    {
-        kill(pid, SIGTERM);
-        waitpid(pid, &status, 0);
     }
 }
 
@@ -305,15 +249,13 @@ bool Authentication::login(const char *name, const char *pass)
     }
 
     pid_t pid = fork();
-    int tty = 2; // todo: need to organize this hardcoded values
+    int tty = 1; // todo: need to organize this hardcoded values
+    result = initgroups(pw->pw_name, pw->pw_gid);
     if (pid == 0)
     {
-        int result = initgroups(pw->pw_name, pw->pw_gid);
-
         if (result != 0)
         {
             Logger(1, "initgroups: failed");
-            // todo: remove this for testing
             // return false;
         }
 
@@ -338,37 +280,21 @@ bool Authentication::login(const char *name, const char *pass)
         snprintf(vt, 5, "vt%d", tty);
 
         init_env(pw);
+        env_xdg(tty_id);
+        reset_terminal(pw);
 
         char **env = pam_getenvlist(pam_handle);
         for (uint16_t i = 0; env && env[i]; i++) // make sure env not empty &&
         {
+            // todo: add all env pam
             putenv(env[i]);
+            Logger(3, env[i]);
         }
 
-        // env_xdg(tty_id, desktop->display_server[desktop->cur] {todo: this is an enum for xorg or wayland ...});
-        env_xdg(tty_id);
-        result = chdir(pw->pw_dir);
-
-        reset_terminal(pw);
-
-        // call xorg function here {yet to be implemented}
         const char *desktop_cmd = "~/.xinitrc"; // todo: need to fix the cmd thingy
         xorg(pw, vt, desktop_cmd);
-
-        /*
-            incase something went wrong, we need to make sure that we delete the ui object
-            before we exit the program
-        */
-
         exit(EXIT_SUCCESS);
     }
-
-    struct utmp entry;
-    add_utmp_entry(&entry, pw->pw_name, pid);
-
-    int status;
-    waitpid(pid, &status, 0);
-    remove_utmp_entry(&entry);
 
     Ui *ui = new Ui();
 
@@ -403,22 +329,13 @@ bool Authentication::login(const char *name, const char *pass)
 
 void Authentication::xauth(const char *display_name, const char *shell, struct passwd *pw)
 {
-
     char *xauthority = (char *)malloc(strlen(pw->pw_dir) + strlen("/.Xauthority") + 1);
     strcpy(xauthority, pw->pw_dir);
     strcat(xauthority, "/.Xauthority");
     setenv("XAUTHORITY", xauthority, 1);
     setenv("DISPLAY", display_name, 1);
 
-    FILE *fp = fopen(xauthority, "ab+");
-
-    if (fp != NULL)
-    {
-        fclose(fp);
-    }
-
     pid_t pid = fork();
-
     if (pid == 0)
     {
         char cmd[1024];
@@ -444,34 +361,4 @@ void Authentication::env_xdg(const char *tty_id)
     setenv("XDG_SEAT", "seat0", 0);
     setenv("XDG_VTNR", tty_id, 0);
     setenv("XDG_SESSION_TYPE", "x11", 0);
-}
-
-void Authentication::add_utmp_entry(struct utmp *entry, char *username, pid_t display_pid)
-{
-    entry->ut_type = USER_PROCESS;
-    entry->ut_pid = display_pid;
-    strcpy(entry->ut_line, ttyname(STDIN_FILENO) + strlen("/dev/"));
-
-    /* only correct for ptys named /dev/tty[pqr][0-9a-z] */
-    strcpy(entry->ut_id, ttyname(STDIN_FILENO) + strlen("/dev/tty"));
-
-    time((long int *)&entry->ut_time);
-
-    strncpy(entry->ut_user, username, UT_NAMESIZE);
-    memset(entry->ut_host, 0, UT_HOSTSIZE);
-    entry->ut_addr = 0;
-    setutent();
-
-    pututline(entry);
-}
-
-void Authentication::remove_utmp_entry(struct utmp *entry)
-{
-    entry->ut_type = DEAD_PROCESS;
-    memset(entry->ut_line, 0, UT_LINESIZE);
-    entry->ut_time = 0;
-    memset(entry->ut_user, 0, UT_NAMESIZE);
-    setutent();
-    pututline(entry);
-    endutent();
 }
